6e80085d0dbb63493db48c0d592c8e9cc6c0f020
max <max.legnar@unreadable.eu>
2018-11-29 16:34:08 +0100

    fertig... glaub ich

--- 1/A3_exercises.m
+++ 1/A3_exercises.m
@@ -1,59 +1,52 @@
 % load image:
 original_img = imread('RandomImage.png');
 
-% get red, green and blue channels:
-img_red = original_img(:,:,1);
-img_green = original_img(:,:,2);
-img_blue = original_img(:,:,3);
+img_with_rectangle=original_img;
+img_with_rectangle([1:100],[1:100],:) = 0;
 
-%...LESEZEICHEN!!!
-original_img_with_rectangle=original_img;
-original_img_with_rectangle([1:100],[1:100],:) = 0;
-
-img_red_with_rectangle=img_red;
+img_red_with_rectangle=original_img(:,:,1);
 img_red_with_rectangle(1:100,1:100) = 0;
 
-img_green_with_rectangle= img_green;
+img_green_with_rectangle= original_img(:,:,2);
 img_green_with_rectangle(1:100,1:100) = 0;
 
-img_blue_with_rectangle=img_blue;
+img_blue_with_rectangle=original_img(:,:,3);
 img_blue_with_rectangle(1:100,1:100) = 0;
 
-%Plot stuff:
-
+%plot:
 subplot(2,4,1);
 imshow(original_img);
-title("original Image");
+title("original");
 
 subplot(2,4,2);
-imshow(img_red);
+imshow(original_img(:,:,1));
 title("Red channel");
 
 subplot(2,4,3);
-imshow(img_green);
+imshow(original_img(:,:,2));
 title("Green channel");
 
 subplot(2,4,4);
-imshow(img_blue);
+imshow(original_img(:,:,3));
 title("Blue channel");
 
 subplot(2,4,5);
-imshow(original_img_with_rectangle);
+imshow(img_with_rectangle);
 title("Image with rectangle");
 
 subplot(2,4,6);
 imshow(img_red_with_rectangle);
-title("Red channel \nwith rectangle");
+title("Red channel with rectangle");
 
 subplot(2,4,7);
 imshow(img_green_with_rectangle);
-title("Green channel\nwith rectangle");
+title("Green channel with rectangle");
 
 subplot(2,4,8);
 imshow(img_blue_with_rectangle);
-title("Blue channel\nwith rectangle");
+title("Blue channel with rectangle");
 
-% Save plot
+% Save
 print('A3_image_bit_planes.png')
 
 
