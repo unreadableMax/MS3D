5c6d8903081a7e92694a1257c0e6330f606f4622
max <max.legnar@unreadable.eu>
2018-11-29 22:16:39 +0100

    sadd

--- 1/A3_exercises.m
+++ 1/A3_exercises.m
@@ -1,49 +1,50 @@
-% load image:
-original_img = imread('RandomImage.png');
+% load original image:
+img = imread('A3_image.png');
 
-img_with_rectangle=original_img;
-img_with_rectangle([1:100],[1:100],:) = 0;
+# draw rectangle into original img
+img_rect=img;
+img_rect([1:100],[1:100],:) = 0;
 
-img_red_with_rectangle=original_img(:,:,1);
-img_red_with_rectangle(1:100,1:100) = 0;
+img_r_rect=img(:,:,1);
+img_r_rect(1:100,1:100) = 0;
 
-img_green_with_rectangle= original_img(:,:,2);
-img_green_with_rectangle(1:100,1:100) = 0;
+img_g_rect= img(:,:,2);
+img_g_rect(1:100,1:100) = 0;
 
-img_blue_with_rectangle=original_img(:,:,3);
-img_blue_with_rectangle(1:100,1:100) = 0;
+img_b_rect=img(:,:,3);
+img_b_rect(1:100,1:100) = 0;
 
 %plot:
 subplot(2,4,1);
-imshow(original_img);
+imshow(img);
 title("original");
 
 subplot(2,4,2);
-imshow(original_img(:,:,1));
+imshow(img(:,:,1));
 title("Red channel");
 
 subplot(2,4,3);
-imshow(original_img(:,:,2));
+imshow(img(:,:,2));
 title("Green channel");
 
 subplot(2,4,4);
-imshow(original_img(:,:,3));
+imshow(img(:,:,3));
 title("Blue channel");
 
 subplot(2,4,5);
-imshow(img_with_rectangle);
+imshow(img_rect);
 title("Image with rectangle");
 
 subplot(2,4,6);
-imshow(img_red_with_rectangle);
+imshow(img_r_rect);
 title("Red channel with rectangle");
 
 subplot(2,4,7);
-imshow(img_green_with_rectangle);
+imshow(img_g_rect);
 title("Green channel with rectangle");
 
 subplot(2,4,8);
-imshow(img_blue_with_rectangle);
+imshow(img_b_rect);
 title("Blue channel with rectangle");
 
 % Save
