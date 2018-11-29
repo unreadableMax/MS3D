8d144543dff7e80a35c31357a600e7c86a7fdc29
max <max.legnar@unreadable.eu>
2018-11-29 10:50:37 +0100

    pipapo

--- 1/A3_exercises.m
+++ 1/A3_exercises.m
@@ -1,51 +1,56 @@
-% Load image
-img_with_rectangle = img = imread('A3_image.png');
+% load image:
+original_img = imread('RandomImage.png');
 
-% Split image-channels into grayscale images
-red_img_with_rectangle = red_img = img(:,:,1);
-green_img_with_rectangle = green_img = img(:,:,2);
-blue_img_with_rectangle = blue_img = img(:,:,3);
+% get red, green and blue channels:
+img_red = original_img(:,:,1);
+img_green = original_img(:,:,2);
+img_blue = original_img(:,:,3);
 
-% Make a black rectangle by setting values in its range to 0
-% Note that the "image coordinate system" is luckily in the upper left corner already
-img_with_rectangle(1:100,1:100,:) = 0;
-red_img_with_rectangle(1:100,1:100) = 0;
-green_img_with_rectangle(1:100,1:100) = 0;
-blue_img_with_rectangle(1:100,1:100) = 0;
+%...LESEZEICHEN!!!
+original_img_with_rectangle=original_img;
+original_img_with_rectangle([1:100],[1:100],:) = 0;
 
-%Plot each image with and without a black rectangle
-figure(1);
+img_red_with_rectangle=img_red;
+img_red_with_rectangle(1:100,1:100) = 0;
+
+img_green_with_rectangle= img_green;
+img_green_with_rectangle(1:100,1:100) = 0;
+
+img_blue_with_rectangle=img_blue;
+img_blue_with_rectangle(1:100,1:100) = 0;
+
+%Plot stuff:
 
 subplot(2,4,1);
-imshow(img);
-title("Image used");
+imshow(original_img);
+title("original Image");
 
 subplot(2,4,2);
-imshow(red_img);
+imshow(img_red);
 title("Red channel");
 
 subplot(2,4,3);
-imshow(green_img);
+imshow(img_green);
 title("Green channel");
 
 subplot(2,4,4);
-imshow(blue_img);
+imshow(img_blue);
 title("Blue channel");
 
 subplot(2,4,5);
-imshow(img_with_rectangle);
+imshow(original_img_with_rectangle);
 title("Image with rectangle");
 
 subplot(2,4,6);
-imshow(red_img_with_rectangle);
+imshow(img_red_with_rectangle);
 title("Red channel \nwith rectangle");
 
 subplot(2,4,7);
-imshow(green_img_with_rectangle);
+imshow(img_green_with_rectangle);
 title("Green channel\nwith rectangle");
 
 subplot(2,4,8);
-imshow(blue_img_with_rectangle);
+imshow(img_blue_with_rectangle);
 title("Blue channel\nwith rectangle");
 
 % Save plot
